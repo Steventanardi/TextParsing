6651eb7ac495b32754aeaac4e2d7f1b0eb7f4fe7
Steven Tanardi 林源茂 <stevntbank77@gmail.com>
2024-05-22 01:34:29 +0800

    s

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ function parseText() {
     const parsedResultDiv = document.getElementById('parsedResult');
 
     const parsedLines = inputText.split('\n').map(line => {
-        const dateTimeAgentPattern = /^\d{1,2}\/\d{1,2}\/\d{2}, \d{1,2}:\d{2} - .+:/;
-        if (dateTimeAgentPattern.test(line)) {
+        const dateTimeAgentPattern = /^\d{1,2}\/\d{1,2}\/\d{2}, \d{1,2}:\d{2} - (.+?):/;
+        const match = line.match(dateTimeAgentPattern);
+        if (match) {
             const [dateTime, agent] = line.split(' - ');
             const [date, time] = dateTime.split(', ');
-            return `<strong>Date:</strong> ${date} <strong>Time:</strong> ${time} <strong>Agent:</strong> ${agent}`;
+            return `<strong>Date:</strong> ${date} <strong>Time:</strong> ${time} <strong>Agent:</strong> ${match[1]}`;
         }
-        return line;
-    });
+        return '';
+    }).filter(line => line !== '');
 
     parsedResultDiv.innerHTML = parsedLines.join('<br>');
 }
